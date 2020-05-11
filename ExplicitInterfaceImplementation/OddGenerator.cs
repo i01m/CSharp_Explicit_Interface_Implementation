e602abdbc7e7caedab888d9798786a83388bdefc
unknown <i.p.m.a.n.03.31.87@gmail.com>
2020-05-11 14:31:09 -0500

    first commit

---
+++ ExplicitInterfaceImplementation/OddGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExplicitInterfaceImplementation
+{
+    class OddGenerator : IEnumerable<int>
+    {
+        //since IEnumerable<int> is intereted from IEnumerable, it must implement 2 GetEnumerator methods
+        //from both interfaces and those both method have the same signatures, so we need to implement
+        //one of them EXPLICITLY using the interface type (second method)
+        public IEnumerator<int> GetEnumerator()
+        {
+            int i = 1;
+            yield return i;
+            while (true)
+            {
+                i += 2;
+                yield return i;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() //EXPLICITLY implemented, its hidded from the class and
+            //can be accessed only by casting class instance into the interface
+        {
+            return this.GetEnumerator(); //returning the previous method
+        }
+    }
+}
---
+++ ExplicitInterfaceImplementation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+
+namespace ExplicitInterfaceImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Odd Numbers");
+
+            var generator = new OddGenerator();
+            foreach (var odd in generator)
+            {
+                if (odd > 50)
+                    break;
+                WriteLine(odd);
+            }
+
+            Read();
+        }
+    }
+}
